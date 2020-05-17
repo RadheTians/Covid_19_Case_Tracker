53bbfc23f82b3dd812abda2fe8f9b691f8e003dc
RadheTians <radheramantiwari87@gmail.com>
2020-05-17 16:31:03 +0530

    Covid Tracker

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def search_state():
         return render_template('index.html',data=data,state=state,active=active,confirmed=confirmed,recovered=recovered,deaths=deaths)
 
 if __name__== '__main__':
-    app.run(debug=True)
+    app.run()
