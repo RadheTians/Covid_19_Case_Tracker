7ef85f1d78d8464c4c4710c4d76bbc9085dfbbc4
RadheTians <radheramantiwari87@gmail.com>
2020-05-17 16:58:17 +0530

    first commit

--- app.py
+++ app.py
@@ -56,33 +56,6 @@ def index():
      data = covid.get_status_by_country_name(country)
      return render_template('index.html',data=data,active=active,confirmed=confirmed,recovered=recovered,deaths=deaths)
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
-
-'''from flask import Flask,request,render_template
-from covid import Covid
-from covid_india import states
-from datetime import datetime
-
- 
-app = Flask(__name__)
- 
-@app.route('/')
-
-def home_view(): 
-        return "<h1>Welcome to Geeks for Geeks</h1>"
-
-def home():
-     covid = Covid(source="worldometers")
-     country = "India"
-     active = covid.get_total_active_cases()
-     confirmed = covid.get_total_confirmed_cases()
-     recovered = covid.get_total_recovered()
-     deaths = covid.get_total_deaths()
-     data = covid.get_status_by_country_name(country)
-     print(data)
-     return render_template('index.html',data=data,active=active,confirmed=confirmed,recovered=recovered,deaths=deaths)
 
 @app.route('/country/',methods=["POST"])
 def search_country():
@@ -109,5 +82,9 @@ def search_state():
         deaths = covid.get_total_deaths()
         return render_template('index.html',data=data,state=state,active=active,confirmed=confirmed,recovered=recovered,deaths=deaths)
 
-if __name__== '__main__':
-    app.run()'''
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
+
+
+
