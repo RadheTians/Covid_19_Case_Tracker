dcba0d69d504d8a96016275f1ef15c36411a6fd7
RadheTians <radheramantiwari87@gmail.com>
2020-05-17 16:26:00 +0530

    Covid Tracker

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
  
 @app.route('/')
 
+def home_view(): 
+        return "<h1>Welcome to Geeks for Geeks</h1>"
+
 def home():
      covid = Covid(source="worldometers")
      country = "India"
